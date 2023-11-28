ac99a9f991dd0dcee69bfb208d9a5fccaee8eff6
Yasmin Lopes <yclopes27@gmail.com>
2023-11-28 16:51:37 -0300

    feat(helpers): add messages

--- app/Helpers/messages/messages.helper.ts
+++ app/Helpers/messages/messages.helper.ts
@@ -2,5 +2,9 @@ export const messagesHelper = {
   EMAIL_EXISTS: "E-mail em uso.",
 
   USERNAME_EXISTS: "Oops! Esse nome de usuário já está em uso.",
+
+  USER_NOT_FOUND: "Usuário não encontrado.",
+
+  BAD_REQUEST: "Oops! Algo deu errado. Tente novamente mais tarde."
   
 };
