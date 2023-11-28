2eeef0b5ca073b127ce935c7a9a3ee34448513fe
Yasmin Lopes <yclopes27@gmail.com>
2023-11-28 16:52:04 -0300

    feat(users): update user data

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -30,7 +30,10 @@ export default class UsersController {
   
     const user = await User.create(body)
   
-    return { user }
+    return { 
+      message: 'Cadastro realizado com sucesso!',
+      user
+     }
   }
 
   public async show({ params }: HttpContextContract) {
@@ -39,9 +42,35 @@ export default class UsersController {
     return { user }
   }
 
-  public async edit({}: HttpContextContract) {}
+  public async edit({}: HttpContextContract) {
+  }
 
-  public async update({}: HttpContextContract) {}
+  public async update({ params, request, response }: HttpContextContract) {
+    try {
+      const user = await User.findOrFail(params.id)
+  
+      const body = request.only(['name', 'last_name', 'email', 'password'])
+
+      if (!body.name || !body.last_name || !body.email || !body.password) {
+        return response.status(400).send({ error: messagesHelper.BAD_REQUEST })
+      }
+  
+      const emailExists = await User.findBy('email', body.email)
+      
+      if (emailExists) {
+        return response.status(403).send({ error:  messagesHelper.EMAIL_EXISTS })
+      }
+      
+      await user.merge(body).save()
+  
+      return {
+        message: 'Editado com sucesso',
+        user
+      }
+    } catch {
+      return response.status(404).send({ error: messagesHelper.USER_NOT_FOUND })
+    }
+  }
 
   public async destroy({}: HttpContextContract) {}
 }
